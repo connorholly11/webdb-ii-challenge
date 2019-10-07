4365f470eca776ba3bad2738c7c87a364a706772
Connor Holly <connorholly11@gmail.com>
2019-10-07 18:42:39 -0400

    weird errors for knexfile

--- seeds/01-cars.js
+++ seeds/01-cars.js
@@ -1,13 +1,16 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return (
+    knex("table_name")
+      //truncate resets the primary key everytime
+      .truncate()
+      .then(function() {
+        // Inserts seed entries
+        return knex("table_name").insert([
+          { id: 1, VIN: "VIN 1", model: "GMC", make: 2012, mileage: 15000 },
+          { id: 1, VIN: "VIN 2", model: "lambo", make: 2016, mileage: 15123 },
+          { id: 1, VIN: "VIN 3", model: "porsche", make: 2011, mileage: 1 }
+        ]);
+      })
+  );
 };
